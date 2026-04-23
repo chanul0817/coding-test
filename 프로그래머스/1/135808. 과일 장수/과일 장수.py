1f5bbc77226d605716bb221763956c6c21104b3e
chanul0817 <s24006@gsm.hs.kr>
2026-04-23 10:41:20 +0900

    [level 1] Title: 과일 장수, Time: 166.12 ms, Memory: 20.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.py
@@ -0,0 +1,14 @@
+def solution(k, m, score):
+    score.sort(reverse=True)
+    
+    answer = 0
+    
+    for i in range(0, len(score), m):
+        box = score[i:i+m]
+        
+        if len(box) < m:
+            break
+        
+        answer += min(box) * m
+    
+    return answer
